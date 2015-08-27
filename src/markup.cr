3911f75ef6bbcdf87c73fa0de72f8ae738c68c0f
Karthik T <karthikt.holmes+github@gmail.com>
2015-08-27 13:42:56 +0800

    Fix issue with stylesheet not getting added

--- src/markup.cr
+++ src/markup.cr
@@ -2,17 +2,16 @@ require "markdown"
 class Markup
   def generate_html(markdown_path, css_path)
     html = Markdown.to_html(File.read(markdown_path))
-    add_stylesheet!(html, css_path)
-    add_head!(html)
+    html = add_head(html, css_path)
+
   end
 
-  def add_head!(html)
-    head = "\n<head>\n<meta charset='utf-8'>\n</head>\n"
+  def add_head(html, css_path)
+    head = "\n<head>\n<meta charset='utf-8'>\n#{stylesheet(css_path)}</head>\n"
     head + html
   end
 
-  def add_stylesheet!(html, css_path)
-    style = "\n<style>#{File.read(css_path)}</style>\n"
-    style + html
+  def stylesheet(css_path)
+    "\n<style>#{File.read(css_path)}</style>\n"
   end
 end
